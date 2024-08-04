6be48a076c6cac293209b9532eb742440fd4573a
Fazle Arefin <fazlearefin@yahoo.com>
2024-08-04 13:30:30 +1000

    Add option to list supported file types
    - `-l` to list supported file type signatures
    - docstrings
    - more specific exceptions
    - pylint rating improvements

--- magic_bitten_file.py
+++ magic_bitten_file.py
@@ -15,35 +15,49 @@ FILE_SIGNATURES_HEX = {
     "png": [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52],
 }
 
+def list_signatures():
+    """Prints all the available file type signatures."""
+    for filetype in FILE_SIGNATURES_HEX:
+        print(filetype)
 
 def prepend_magic_bytes(filename, filetype):
+    """Prepends the magic bytes of the specified file type to the given file."""
     try:
-        # check if the file already has the magic bytes of intended type
         with open(filename, "rb") as file:
             signature = file.read(len(FILE_SIGNATURES_HEX[filetype]))
             if signature == bytes(FILE_SIGNATURES_HEX[filetype]):
-                raise Exception(f"This file {filename} is already of type {filetype}")
+                raise ValueError(f"This file {filename} is already of type {filetype}")
 
         with open(filename, "rb+") as file:
             content = file.read()
             file.seek(0)
             file.write(bytes(FILE_SIGNATURES_HEX[filetype]))
             file.write(content)
-    except Exception as exception:
-        sys.exit(exception)
-
+    except ValueError as value_error:
+        sys.exit(value_error)
+    except (OSError, IOError) as error:
+        sys.exit(f"Error reading or writing to file {filename}: {error}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Set file options")
     parser.add_argument(
         "-f",
         "--filetype",
-        required=True,
         choices=list(FILE_SIGNATURES_HEX),
         help="Insert magic bytes of this file type",
     )
-    parser.add_argument("filename", type=str, help="the filename")
+    parser.add_argument(
+        "-l",
+        "--list",
+        action="store_true",
+        help="List all the file signatures"
+    )
+    parser.add_argument("filename", type=str, nargs='?', help="the filename")
     args = parser.parse_args()
 
-    if args.filetype:
+    if args.list:
+        list_signatures()
+    elif args.filetype and args.filename:
         prepend_magic_bytes(args.filename, args.filetype)
+    else:
+        parser.print_help()
